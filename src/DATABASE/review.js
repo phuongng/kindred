3f33389404bc19fce83d2910c31387eb3a4e177c
Carr Lois <131558625+carabeef@users.noreply.github.com>
2024-03-24 19:02:10 -0400

    add model files (appt, caregiver, client, review)

---
+++ src/DATABASE/review.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema({
+    reviewID: {
+        type: mongoose.Types.ObjectId
+    },
+    clientID: {
+        type: String,
+        ref: 'client',
+    },
+    caregiverID: {
+        type: String,
+        ref: 'caregiver',
+    },
+    appointmentID: {
+        type: String,
+        ref: 'appointment',
+    },
+    rating: {
+        type: Number,
+        required: true,
+        maxlength: 5,
+        minlength: 1
+    },
+    comment: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: () => Date.now(),
+    }
+});
+
+module.exports = mongoose.model('review', reviewSchema);
